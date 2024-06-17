805f12cfde2aede580e9469aacfaba13c5344d10
Mutiulrasool <mutiulrasool769@gmail.com>
2024-06-17 14:38:18 -0700

    Function's assignment is completed

--- lastAssignment.js
+++ lastAssignment.js
@@ -54,37 +54,80 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("task-4-p").style.fontWeight = "bold"
     }
     document.querySelector("#task-4-btn").addEventListener("click", myFunction4);
-    
+
     function myFunction5() {
         var width = parseInt(prompt("plese enter the width in CM of triangle(value should be in numbers)"));
         var hight = parseInt(prompt("plese enter the hight in CM of triangle(value should be in numbers)"));
         var resultToShow = () => {
             return width * hight;
-        } 
-        
-    document.getElementById("task-5-p").innerHTML = "The total area of the triangle is: " + resultToShow(width, hight);
+        }
+
+        document.getElementById("task-5-p").innerHTML = "The total area of the triangle is: " + resultToShow(width, hight);
         console.log(resultToShow(width, hight));
     }
 
     document.querySelector("#task-5-btn").addEventListener("click", myFunction5);
-    
+
     function myFunction6() {
         var userWord = prompt("Enter any word to check if its a palindrome word or not");
-        // for(let i = 0; i < userWord.length -1; i++){
-        var collected  = "";
-            for(var j = userWord.length -1; j >= 0 ; j-- ){
-                    // collected += userWord;
-                    console.log(collected);
-                }
-            
-            // }
 
-        // var checkInReturn = userWord.slice( userWord.length, -4)
-        // console.log(userWord);
-        // console.log(checkInReturn);
-        // for(x of userWord.length) {
-        //     console.log(x)
-        // }
+        var collected = "";
+        for (var j = userWord.length - 1; j >= 0; j--) {
+            collected += userWord[j];
+            console.log(collected);
         }
-        document.querySelector("#task-6-btn").addEventListener("click", myFunction6);
+        var finalReversWord = "";
+        for (x of collected) {
+            finalReversWord += x;
+            console.log(finalReversWord);
+        }
+        if (userWord === finalReversWord) {
+            alert("yes! its a palindrome word")
+        }
+        else {
+            alert("No! its not a palidrome word")
+        }
+    }
+    document.querySelector("#task-6-btn").addEventListener("click", myFunction6);
+
+    function myFunction7() {
+        var sentence = "the quick brown fox";
+        var word = sentence.split(" ");
+        console.log(typeof word)
+        for (let i = 0; i < word.length; i++) {
+            word[i] = word[i][0].toUpperCase() + word[i].substring(1);
+        }
+        document.getElementById("task-7-p").innerHTML = word;
+    }
+
+    document.querySelector("#task-7-btn").addEventListener("click", myFunction7);
+
+    function myFunction8() {
+        var fullSentence = "Web Development Tutorial";
+        var convertToArray = fullSentence.split(" ");
+        console.log(convertToArray);
+        var longestWord = " ";
+        for (let i = 0; i < convertToArray.length; i++) {
+            if (convertToArray[i].length > longestWord.length) {
+                longestWord = convertToArray[i];
+            }
+        }
+        document.getElementById("task-8-p").innerHTML = "In the paragraph:'Web Development Tutorial' " + "<br>" + "The longest word is: " + longestWord;
+        console.log(longestWord)
+    }
+    document.querySelector("#task-8-btn").addEventListener("click", myFunction8);
+
+    function myFunction9(wordByUser, specifiedLetter) {
+        var wordByUser = prompt("Please give us a word");
+        var specifiedLetter = prompt("write a letter to find in the word you provided");
+
+        var findingTheLetter = wordByUser.indexOf(specifiedLetter);
+        document.getElementById("task-9-p").innerHTML = findingTheLetter;
+
+    }
+    document.querySelector("#task-9-btn").addEventListener("click", myFunction9);
+
+
+
+
 });
